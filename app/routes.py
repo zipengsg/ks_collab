e077308205d143c3de27b5a0f2764b7b2d2072e0
Chi Feng <email.chi.feng@gmail.com>
2018-10-03 21:29:06 -0400

    Added static assets and refactored routes to behave more like a rest API endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, send_from_directory, jsonify
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
@@ -11,27 +11,23 @@ from app.utilities import DatabaseCleaner
 # pylint: disable=singleton-comparison
 # pylint: disable=no-self-use
 
+# host static content in the assets directory
+# TODO: don't host this using flask since it's super inefficient (use nginx instead)
+@app.route('/assets/<path:path>')
+def send_assets(path):
+    return send_from_directory('assets', path)
+
+@app.route('/assets-landing/<path:path>')
+def send_assets_landing(path):
+    return send_from_directory('assets-landing', path);
+
 @app.route('/')
 @app.route('/index')
-# @login_required
 def index():
     """
-    index function details all the potential interactions the client may
-        have with the /index page of the web app.
-    At the moment, index function acts as a placeholder for the home page.
+    home page
     """
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html", title='Home Page', posts=posts)
+    return render_template("frontend/landing.html", title='Home Page')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -53,7 +49,7 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('frontend/login-and-register.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
@@ -81,54 +77,59 @@ def register():
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('login-and-register.html', title='Register', form=form)
 
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    """
-    test function presents a temporary admin interface that allows developers
-        to call functions and conduct unit testing.
-    """
-    form = TestingButtonsForm()
+
+@app.route('/admin', methods=['GET'])
+def admin():
+    return render_template('backend/admin.html', title='Admin')
+
+@app.route('/admin/sources/tweets/update', methods=['POST'])
+def update_tweets():
     twitter_scraper = TwitterScraper()
+    data = twitter_scraper.update_source()
+    return jsonify(data);
+
+@app.route('/admin/sources/newsplease/crawl', methods=['POST'])
+def crawl_newsplease():
     newsplease_scraper = NewspleaseScraper()
+    data = newsplease_scraper.crawl_source()
+    return jsonify(data);    
+
+@app.route('/admin/sources/newsapi/request', methods=['POST'])
+def request_newsapi():
     newsapi_api = NewsApiApi()
+    data = newsapi_api.request_source(request.form['newsapi_keyword'], int(request.form(newsapi_max_pages)));
+    return jsonify(data);
+
+@app.route('/admin/sources/newsapi/update', methods=['POST'])
+def update_newsapi():
+    newsapi_api = NewsApiApi()
+    data = newsapi_api.update_source();
+    return jsonify(data);
+
+@app.route('/admin/sources/newsapi/crawl', methods=['POST'])
+def crawl_newsapi_fulltext():
+    newsapi_api = NewsApiApi()
+    data = newsapi_api.crawl_newsapi_fulltext();
+    return jsonify(data);
+
+@app.route('/admin/database/articles/remove-fulltext-duplicates', methods=['POST'])
+def remove_fulltext_duplicates():
     database_cleaner = DatabaseCleaner()
+    data = database_cleaner.article_remove_fulltext_duplicates();
+    return jsonify(data);
 
-    if form.validate_on_submit():
-        if form.update_tweets.data:
-            print('updating tweets')
-            twitter_scraper.update_source()
-        
-        if form.crawl_commoncrawl.data:
-            print('crawling commoncrawl')
-            newsplease_scraper.crawl_source()
-        
-        if form.request_newsapi.data:
-            print('requesting newsAPI')
-            newsapi_api.request_source(form.newsapi_keyword_field.data,
-                                       int(form.newsapi_max_pages_field.data))
+@app.route('/admin/database/articles/remove-url-duplicates', methods=['POST'])
+def remove_url_duplicates():
+    database_cleaner = DatabaseCleaner()
+    data = database_cleaner.article_remove_url_duplicates();
+    return jsonify(data);
 
-        if form.update_newsapi.data:
-            print('updating newsAPI')
-            newsapi_api.update_source()
+@app.route('/admin/database/articles/populate-source', methods=['POST'])
+def populate_source_from_articles():
+    database_cleaner = DatabaseCleaner()
+    data = database_cleaner.populate_source_from_articles();
+    return jsonify(data);
 
-        if form.crawl_fulltext_for_newsapi.data:
-            print('crawling content for newsAPI')
-            newsplease_scraper.crawl_newsapi_fulltext()
-            
-        if form.remove_fulltext_duplicates.data:
-            print('removing fulltext duplicates')
-            database_cleaner.article_remove_fulltext_duplicates()
-        
-        if form.remove_url_duplicates.data:
-            print('removing url duplicates')
-            database_cleaner.article_remove_url_duplicates()
 
-        if form.populate_source_from_article.data:
-            print('populating source table from existing articles')
-            database_cleaner.populate_source_from_articles()
-
-        return redirect(url_for('test'))
-
-    return render_template('test.html', title='Test', form=form)
